3f0af0e87b8ca651f8ab47d3e127b088348a426a
Dawid Holisz <dawid.holisz@gmail.com>
2021-02-12 13:29:59 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Nav, Navbar} from "react-bootstrap";
+
+import Geo from "./pages/Geo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="/">Osom PWA</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/geo">Geolocation</Nav.Link>
+                        <Nav.Link href="/offline-notes">Offline notes</Nav.Link>
+                        <Nav.Link href="/camera">Camera</Nav.Link>
+                        <Nav.Link href="/reachability">Reachability</Nav.Link>
+                        <Nav.Link href="/position">Device position</Nav.Link>
+                    </Nav>
+                </Navbar>
+                <main>
+                    <Container>
+                    <Switch>
+                      <Router path="/geo">
+                        <Geo/>
+                      </Router>
+                        <Route>
+                            HomePejdz
+                        </Route>
+                    </Switch>
+                    </Container>
+                </main>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Geo.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {useGeolocation, useAsyncFn} from 'react-use';
+import {Row, Alert, Spinner, Col} from "react-bootstrap";
+
+const Geo = () => {
+    const {loading, longitude, latitude, error} = useGeolocation()
+    const [state, getPlaces] = useAsyncFn(async () => {
+        const response = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${process.env.REACT_APP_GOOGLE_MAP_API}&radius=1000&location=49.8103,19.0129&type=parking,restaurant`)
+        return response.json()
+    }, [])
+
+    if (loading)
+        return <Spinner animation="border" variant="info"/>
+
+    if (error?.message)
+        return <Alert variant="danger">{error.message}</Alert>
+
+    const fetchPlaces = async () =>{
+        const resp = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${process.env.REACT_APP_GOOGLE_MAP_API}&radius=1000&location=49.8103,19.0129&type=parking,restaurant`).then(resp=>resp.json()).then(resp=>resp.results)
+        console.log(resp);
+    }
+
+    return (
+        <Row>
+            <Col>
+                <div>Your position:
+                    <p>{longitude} lng</p>
+                    <p>{latitude} lat</p>
+                    <button onClick={fetchPlaces}>XD</button>
+                </div>
+            </Col>
+            <Col>
+                sds
+            </Col>
+        </Row>
+    );
+};
+
+export default Geo;
